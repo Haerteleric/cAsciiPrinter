91a43a4646a6e26e35609bf5a7bec0861bf00402
Haerteleric <haertelgu@gmail.com>
2023-11-13 20:00:22 +0100

    reworked the way output works

--- inc/asciiPrinter_t.h
+++ inc/asciiPrinter_t.h
@@ -26,13 +26,12 @@
  * SOFTWARE.
  **/
 #include <stddef.h>
+#include <stdarg.h>
 #include <stdbool.h>
 
 #ifndef _ASCII_PRINTER_INCLUDED
 #define _ASCII_PRINTER_INCLUDED
 #endif
-
-typedef unsigned int (* write_func)(char* asciiChars, unsigned int numAsciiChars);
 
 
 #ifdef ASCII_PRINTER_INLINE_IMPLEMENTATION
@@ -62,13 +61,16 @@ inline
 #ifdef ASCII_PRINTER_STATIC_IMPLEMENTATION
 static
 #endif 
-void ascii_putNibbleHex(write_func output, unsigned char nibble)
+unsigned int ascii_putNibbleHex(char * dest, unsigned char nibble)
 #ifdef ASCII_PRINTER_ONLY_PROTOTYPE_DECLARATION
 ;
 #else
 {
     char nibbleAscii = ascii_unsignedValue2HexChar(nibble);
-    output(&nibbleAscii,1);
+    *dest = nibbleAscii;
+
+    //Always One Char
+    return 1;
 }
 #endif // NOT(ASCII_PRINTER_ONLY_PROTOTYPE_DECLARATION)
 
@@ -80,13 +82,16 @@ inline
 #ifdef ASCII_PRINTER_STATIC_IMPLEMENTATION
 static
 #endif 
-void ascii_putByteHex(write_func output, unsigned char byte)
+unsigned int ascii_putByteHex(char * dest, unsigned char byte)
 #ifdef ASCII_PRINTER_ONLY_PROTOTYPE_DECLARATION
 ;
 #else
 {
-    ascii_putNibbleHex(output, (byte & 0b11110000) >> 4);
-    ascii_putNibbleHex(output, byte & 0b00001111);
+    ascii_putNibbleHex(&dest[0], (byte & 0b11110000) >> 4);
+    ascii_putNibbleHex(&dest[1], byte & 0b00001111);
+
+    //Always Two Char
+    return 2;
 }
 #endif // NOT(ASCII_PRINTER_ONLY_PROTOTYPE_DECLARATION)
 
@@ -98,7 +103,7 @@ inline
 #ifdef ASCII_PRINTER_STATIC_IMPLEMENTATION
 static
 #endif 
-void ascii_putUnsignedDecimal(write_func output, unsigned int unsignedNum)
+unsigned int ascii_putUnsignedDecimal(char * dest, unsigned int unsignedNum)
 #ifdef ASCII_PRINTER_ONLY_PROTOTYPE_DECLARATION
 ;
 #else
@@ -108,6 +113,7 @@ void ascii_putUnsignedDecimal(write_func output, unsigned int unsignedNum)
 
 #ifndef ASCII_PRINTER_UNSIGNED_LEADING_ZEROS
     bool wordStarted = false;
+    unsigned int numCharsWritten = 0;
 #endif
 
     while (decMask)
@@ -121,57 +127,203 @@ void ascii_putUnsignedDecimal(write_func output, unsigned int unsignedNum)
         {
             wordStarted = true;
             currentDigit += '0';
-            output(&currentDigit, 1);
+            dest[numCharsWritten++] = currentDigit;
         }
 
         decMask /= 10;
     }
 
 #ifndef ASCII_PRINTER_UNSIGNED_LEADING_ZEROS
+
     if(!wordStarted)
     {
-        output("0", 1);
+        dest[numCharsWritten++] = '0';
     }
+    return numCharsWritten;
+
+#else
+
+    //Always 10 Digits
+    return 10;
+
+#endif // NOT(ASCII_PRINTER_UNSIGNED_LEADING_ZEROS)
+}
+#endif // NOT(ASCII_PRINTER_ONLY_PROTOTYPE_DECLARATION)
+
+
+
+#ifdef ASCII_PRINTER_INLINE_IMPLEMENTATION
+inline
+#endif 
+#ifdef ASCII_PRINTER_STATIC_IMPLEMENTATION
+static
+#endif 
+unsigned int ascii_putHexLittleEndian(char * dest, unsigned char * data, unsigned int size)
+#ifdef ASCII_PRINTER_ONLY_PROTOTYPE_DECLARATION
+;
+#else
+{
+    unsigned int offset = 0;
+    for (size_t i = 0; i < size; i++)
+    {
+        offset += ascii_putByteHex(&dest[offset], data[size-1-i]);
+    }
+
+    return size * 2; //2 Chars per Byte
+}
+#endif // NOT(ASCII_PRINTER_ONLY_PROTOTYPE_DECLARATION)
+
+
+
+#ifdef ASCII_PRINTER_INLINE_IMPLEMENTATION
+inline
+#endif 
+#ifdef ASCII_PRINTER_STATIC_IMPLEMENTATION
+static
+#endif 
+unsigned int ascii_putHexBigEndian(char * dest, unsigned char * data, unsigned int size)
+#ifdef ASCII_PRINTER_ONLY_PROTOTYPE_DECLARATION
+;
+#else
+{
+    unsigned int offset = 0;
+    for (size_t i = 0; i < size; i++)
+    {
+        offset += ascii_putByteHex(&dest[offset], data[i]);
+    }
+
+    return size * 2; //2 Chars per Byte
+}
+#endif // NOT(ASCII_PRINTER_ONLY_PROTOTYPE_DECLARATION)
+
+
+#ifdef ASCII_PRINTER_INLINE_IMPLEMENTATION
+inline
 #endif
-}
-#endif // NOT(ASCII_PRINTER_ONLY_PROTOTYPE_DECLARATION)
-
-
-
-#ifdef ASCII_PRINTER_INLINE_IMPLEMENTATION
-inline
-#endif 
 #ifdef ASCII_PRINTER_STATIC_IMPLEMENTATION
 static
-#endif 
-void ascii_putHexLittleEndian(write_func output, unsigned char * data, unsigned int size)
+#endif
+unsigned int ascii_putString(char * dest, unsigned char * str)
 #ifdef ASCII_PRINTER_ONLY_PROTOTYPE_DECLARATION
 ;
 #else
 {
-    for (size_t i = 0; i < size; i++)
+    unsigned int numCharsWritten = 0;
+    while(*str != '\0')
     {
-        ascii_putByteHex(output, data[size-1-i]);
+        dest[numCharsWritten++] = *str;
+        str++;
     }
+
+    return numCharsWritten;
 }
 #endif // NOT(ASCII_PRINTER_ONLY_PROTOTYPE_DECLARATION)
-
 
 
 #ifdef ASCII_PRINTER_INLINE_IMPLEMENTATION
 inline
-#endif 
+#endif
 #ifdef ASCII_PRINTER_STATIC_IMPLEMENTATION
 static
-#endif 
-void ascii_putHexBigEndian(write_func output, unsigned char * data, unsigned int size)
+#endif
+unsigned int ascii_vsprintf(char * dest, const char * msgFormat, va_list vaList)
 #ifdef ASCII_PRINTER_ONLY_PROTOTYPE_DECLARATION
 ;
 #else
 {
-    for (size_t i = 0; i < size; i++)
-    {
-        ascii_putByteHex(output, data[i]);
-    }
+  unsigned int parsePos = 0;
+  unsigned int numCharsWritten = 0;
+
+  parsePos = 0;
+  //Generate Final Message
+  while(msgFormat[parsePos] != '\0')
+  {
+      if(msgFormat[parsePos] == '%')
+      {
+          parsePos++; //skip over wildcard escape char
+
+          switch(msgFormat[parsePos])
+          {
+          case 'u':
+            {
+              unsigned int decValue = va_arg(vaList, unsigned int);
+              numCharsWritten += ascii_putUnsignedDecimal(&dest[numCharsWritten], decValue);
+            }break;
+
+          case 'x':
+            {
+              unsigned int hexValue = va_arg(vaList, unsigned int);
+              numCharsWritten += ascii_putHexLittleEndian(&dest[numCharsWritten], (unsigned char * )&hexValue ,sizeof(unsigned int));
+            }break;
+
+          case 's':
+            {
+              numCharsWritten += ascii_putString(&dest[numCharsWritten],va_arg(vaList, char *));
+            }break;
+
+          default:
+            {
+            }break;
+          }
+
+          parsePos++; //skip over wildcard identifier char
+      }
+      else
+      {
+          dest[numCharsWritten++] = msgFormat[parsePos++];
+      }
+  }
+
+  return numCharsWritten;
 }
 #endif // NOT(ASCII_PRINTER_ONLY_PROTOTYPE_DECLARATION)
+
+
+#ifdef ASCII_PRINTER_INLINE_IMPLEMENTATION
+inline
+#endif
+#ifdef ASCII_PRINTER_STATIC_IMPLEMENTATION
+static
+#endif
+unsigned int ascii_getNumWildcards(const char * msgFormat)
+#ifdef ASCII_PRINTER_ONLY_PROTOTYPE_DECLARATION
+;
+#else
+{
+  unsigned int parsePos = 0;
+  unsigned int numWildcards = 0;
+
+  while(msgFormat[parsePos] != '\0')
+  {
+      if(msgFormat[parsePos++] == '%')
+      {
+          numWildcards++;
+      }
+  }
+
+  return numWildcards;
+}
+#endif // NOT(ASCII_PRINTER_ONLY_PROTOTYPE_DECLARATION)
+
+
+#ifdef ASCII_PRINTER_INLINE_IMPLEMENTATION
+inline
+#endif
+#ifdef ASCII_PRINTER_STATIC_IMPLEMENTATION
+static
+#endif
+unsigned int ascii_sprintf(char * dest, const char * msgFormat, ...)
+#ifdef ASCII_PRINTER_ONLY_PROTOTYPE_DECLARATION
+;
+#else
+{
+  va_list vaList;
+  va_start(vaList, msgFormat);
+
+  return ascii_vsprintf(dest, msgFormat, vaList);
+}
+#endif // NOT(ASCII_PRINTER_ONLY_PROTOTYPE_DECLARATION)
+
+
+
+
